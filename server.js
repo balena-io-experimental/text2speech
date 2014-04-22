e4b2056b4fdd050edbe60fe20affd7eefc21e361
Petros Angelatos <petrosagg@gmail.com>
2014-04-22 16:22:46 +0100

    Drop festival dependency and use google tts to get an mp3 stream

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
-var Say = require('say');
-Say.speak('Alex', 'Say hello to my little friend.');
+var request = require('request');
+var Lame = require('lame');
+var Speaker = require('speaker');
+
+var text = 'Say hello to my little friend';
+
+var url = 'http://translate.google.com/translate_tts?tl=en&q=' + encodeURIComponent(text);
+request(url).pipe(new Lame.Decoder).pipe(new Speaker);
