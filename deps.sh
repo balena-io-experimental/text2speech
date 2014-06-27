6369e345f6e96b35a91003aa5a45610e23f44cb2
Praneeth Bodduluri <praneeth@bufferlabs.in>
2014-06-27 15:35:24 +0530

    Alsa fixes to set the default sound levels

--- deps.sh
+++ deps.sh
@@ -1,1 +1,6 @@
-apt-get install -y libasound2-dev
+apt-get install -y alsa-utils libasound2-dev
+
+echo "#!/bin/bash" > /usr/bin/sound_start
+echo "amixer cset numid=3 1" >> /usr/bin/sound_start
+echo "amixer -c 3 set PCM 1dB+" >> /usr/bin/sound_start
+chmod a+x /usr/bin/sound_start
--- server.js
+++ server.js
@@ -2,7 +2,16 @@ var request = require('request');
 var Lame = require('lame');
 var Speaker = require('speaker');
 
+var fs = require('fs'),
+          spawn = require('child_process').spawn,
+	             out = fs.openSync('./out.log', 'a'),
+		                     err = fs.openSync('./out_err.log', 'a');
+
 var text = 'Say hello to my little friend';
+var child = spawn('/usr/bin/sound_start', [], {
+            detached: true,
+            stdio: [ 'ignore', out, err ]
+          });
 
 var url = 'http://translate.google.com/translate_tts?tl=en&q=' + encodeURIComponent(text);
 request(url).pipe(new Lame.Decoder).pipe(new Speaker);
