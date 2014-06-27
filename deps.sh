0d8f12902e084583fabeb88296763cf0301a9b35
Praneeth Bodduluri <praneeth@bufferlabs.in>
2014-06-27 16:31:41 +0530

    Sound Start moved to a different file

--- deps.sh
+++ deps.sh
@@ -1,6 +1,2 @@
 apt-get install -y alsa-utils libasound2-dev
-
-echo "#!/bin/bash" > /usr/bin/sound_start
-echo "amixer cset numid=3 1" >> /usr/bin/sound_start
-echo "amixer -c 3 set PCM 1dB+" >> /usr/bin/sound_start
-chmod a+x /usr/bin/sound_start
+mv sound_start /usr/bin/sound_start
