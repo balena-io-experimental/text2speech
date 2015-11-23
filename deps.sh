53262615de054101380ce426c34b5df378b2377e
Lorenzo Stoakes <lstoakes@gmail.com>
2015-11-23 17:59:56 +0000

    Fix issue with recent node running deps.sh AFTER dependencies are installed.

--- deps.sh
+++ deps.sh
@@ -5,3 +5,6 @@ set -o pipefail
 
 apt-get update
 apt-get install -y alsa-utils libasound2-dev
+
+# Recent node now installs dependencies before running preinstall scripts :(
+npm install speaker@~0.2.6
